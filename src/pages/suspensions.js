d942cbbcc3f8300b6219ecdd53bf79593dea8f1b
Aktonen <aku.tolonen1999@hotmail.fi>
2024-02-28 09:49:44 +0200

    Placed the info inside cards

--- src/pages/suspensions.js
+++ src/pages/suspensions.js
@@ -3,6 +3,13 @@ import { useEffect, useState } from 'react';
 import { db } from '../firebase'
 import { getDocs, collection } from 'firebase/firestore'
 
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 async function fetchDataFromDb() {
   const query = await getDocs(collection(db, "news"))
 
@@ -26,12 +33,39 @@ const Suspensions = () => {
   }, []);
 
   return (
-    <div>
+    <div
+    style={{
+      display: 'inline-block',
+    }}
+    >
       {newsData.map((news) => (
-        <div key={news.id}>
-          <h1>{news.text}</h1>
-          <p>{news.link}</p>
-        </div>
+        <Card
+          variant='outlined'
+          style={{
+            alignContent: 'middle',
+            justifyContent: 'center',
+            marginTop: '10px',
+          }}
+        >
+          <React.Fragment>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Kurinpito
+              </Typography>
+              <Typography variant="h5" component="div">
+                {news.text}
+              </Typography>
+            </CardContent>
+            <CardActions
+              style={{
+                alignContent: 'middle',
+                justifyContent: 'center',
+              }}
+            >
+              <a size="small">liiga.fi{news.link}</a>
+            </CardActions>
+          </React.Fragment>
+        </Card>
       ))}
     </div>
   )
