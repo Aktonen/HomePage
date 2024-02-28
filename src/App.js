694eeae9f93e49009d08c845fc9cc4e629e78188
Aktonen <aku.tolonen1999@hotmail.fi>
2024-02-28 17:47:43 +0200

    Made basic navigation for the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
 import Suspensions from './pages/suspensions';
+import Electricity from './pages/electricity';
+
+import { useState } from 'react';
+import { Button } from '@mui/material';
 
 function App() {
 
+  const [currentPage, setCurrentPage] = useState("home");
+
+  const handleNavigation = (page) => {
+    setCurrentPage(page)
+  };
+
   return (
     <div className="App">
-      <Suspensions />
+      <Button
+        onClick={() => handleNavigation("home")}
+        style={{
+          color: 'black'
+        }}
+      >
+        Home
+      </Button>
+      <Button
+        onClick={() => handleNavigation("electricity")}
+        style={{
+          color: 'black'
+        }}
+      >
+        Electricity
+      </Button>
+      {currentPage === "home" && <Suspensions />}
+      {currentPage === "electricity" && <Electricity />}
     </div>
   );
 }
