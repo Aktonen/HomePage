a0b39f3e5aa9e07994b2ef482d3013338728013c
Aktonen <aku.tolonen1999@hotmail.fi>
2024-03-04 11:13:40 +0200

    Some initial info in the home page.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,39 @@
 import React from 'react'
 
 export default function Home() {
-  return (
-    <div>home</div>
-  )
+	return (
+		<div className="home-page">
+			<header className="hero-section">
+				<h1>Aku Tolonen</h1>
+				<p>Web Developer with a focus on front-end and cybersecurity</p>
+			</header>
+
+			<section className="about-me">
+				<h2>About Me</h2>
+				{/* Your Intro Here */}
+			</section>
+
+			<section className="featured-projects">
+				<h2>Featured Projects</h2>
+				{/* Display 2-3 projects with image, title, short description, link */}
+			</section>
+
+			<footer>
+				<a
+					href='https://github.com/Aktonen'
+					target='_blank'
+					rel='noreferrer'
+				>
+					Github
+				</a>
+				<a
+					href='https://www.linkedin.com/in/aku-tolonen-1ba4a3261/'
+					target='_blank'
+					rel='noreferrer'
+				>
+					LinkedIn
+				</a>
+			</footer>
+		</div>
+	)
 }
