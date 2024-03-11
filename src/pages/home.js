1dfbdcd557bc1b2c16a660fcba710062a8db83f5
Aktonen <aku.tolonen1999@hotmail.fi>
2024-03-11 12:25:50 +0200

    Style changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -16,28 +16,34 @@ export default function Home() {
         </section>
 
         <section className="featured-projects">
-          <h2>Featured Projects</h2>
           <div className='introduction-to-page'>
-            On this web page you can find my projects which I have created to learn something new. Also just to demonstrate my web development skills. Front-end is ReactJS. You can propably notice that design is my weakness but I'm working on it ;)
+            <h2>Featured Projects</h2>
+            <div className='introduction-text'>
+              On this web page you can find my projects which I have created to learn something new. Also just to demonstrate my web development skills. Front-end is ReactJS. You can propably notice that design is my weakness but I'm working on it ;)
+            </div>
           </div>
-          <h3>Suspensions page</h3>
-          <div className='suspensions'>
-            The suspensions page was a project to learn web scraping and how that works.
-            The data on the page is scraped from Liiga.fi website. I have deployed the Python script to a cloud service and when the service is called it runs the script and updates the data to a firebase firestore database.
-            <h4>What I learned:</h4>
-            <ul>
-              <li>Python</li>
-              <li>Web scraping</li>
-              <li>Firestore database</li>
-            </ul>
+          <div className='home-suspensions'>
+            <h3>Suspensions page</h3>
+            <div className='suspensions-text'>
+              The suspensions page was a project to learn web scraping and how that works.
+              The data on the page is scraped from Liiga.fi website. I have deployed the Python script to a cloud service and when the service is called it runs the script and updates the data to a firebase firestore database.
+              <h4>What I learned:</h4>
+              <ul>
+                <li>Python</li>
+                <li>Web scraping</li>
+                <li>Firestore database</li>
+              </ul>
+            </div>
           </div>
-          <h3>Electricity page</h3>
-          <div className='electricity'>
-            The electricity page is a quick project that I made in couple of hours. I found a free API that can get the current (on the hour) or current or upcoming days prices. The page just calls the API there's nothing more to the page.
-            <h4>What I learned:</h4>
-            <ul>
-              <li>API usage</li>
-            </ul>
+          <div className='home-electricity'>
+            <h3>Electricity page</h3>
+            <div className='electricity-text'>
+              The electricity page is a quick project that I made in couple of hours. I found a free API that can get the current (on the hour) or current or upcoming days prices. The page just calls the API there's nothing more to the page.
+              <h4>What I learned:</h4>
+              <ul>
+                <li>API usage</li>
+              </ul>
+            </div>
           </div>
         </section>
 
